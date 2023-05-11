d4d9dc97e8b28dd9f8898cd64bb3c22fd6bd7092
Matthew Leibowitz <mattleibow@live.com>
2023-05-12 04:07:22 +0800

    text boxes

--- src/Controls/src/Core/HandlerImpl/SearchBar/SearchBar.cs
+++ src/Controls/src/Core/HandlerImpl/SearchBar/SearchBar.cs
@@ -1,24 +1,23 @@
 ﻿#nullable disable
+using System;
+
 namespace Microsoft.Maui.Controls
 {
 	public partial class SearchBar
 	{
 		public static IPropertyMapper<ISearchBar, SearchBarHandler> ControlsSearchBarMapper =
-			new PropertyMapper<SearchBar, SearchBarHandler>(SearchBarHandler.Mapper)
-			{
-#if WINDOWS
-				[PlatformConfiguration.WindowsSpecific.SearchBar.IsSpellCheckEnabledProperty.PropertyName] = MapIsSpellCheckEnabled,
-#elif IOS
-				[PlatformConfiguration.iOSSpecific.SearchBar.SearchBarStyleProperty.PropertyName] = MapSearchBarStyle,
-#endif
-				[nameof(Text)] = MapText,
-				[nameof(TextTransform)] = MapText,
-			};
+			new PropertyMapper<SearchBar, SearchBarHandler>(SearchBarHandler.Mapper);
 
 		internal static new void RemapForControls()
 		{
 			// Adjust the mappings to preserve Controls.SearchBar legacy behaviors
-			SearchBarHandler.Mapper = ControlsSearchBarMapper;
+#if WINDOWS
+			SearchBarHandler.Mapper.ReplaceMapping<SearchBar, ISearchBarHandler>(PlatformConfiguration.WindowsSpecific.SearchBar.IsSpellCheckEnabledProperty.PropertyName, MapIsSpellCheckEnabled);
+#elif IOS
+			SearchBarHandler.Mapper.ReplaceMapping<SearchBar, ISearchBarHandler>(PlatformConfiguration.iOSSpecific.SearchBar.SearchBarStyleProperty.PropertyName, MapSearchBarStyle);
+#endif
+			SearchBarHandler.Mapper.ReplaceMapping<SearchBar, ISearchBarHandler>(nameof(Text), MapText);
+			SearchBarHandler.Mapper.ReplaceMapping<SearchBar, ISearchBarHandler>(nameof(TextTransform), MapText);
 
 #if ANDROID
 			SearchBarHandler.CommandMapper.AppendToMapping(nameof(ISearchBar.Focus), MapFocus);
